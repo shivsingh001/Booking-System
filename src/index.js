80130bbcc335244243910419b6d39e3b6d309635
shivsingh001 <shivamkumar0346@gmail.com>
2024-07-10 03:39:55 +0530

    setup the body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+const bodyParser = require("body-parser");
 const express =require("express");
 
 const setupAndStartServer = async ()=>{
 
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(PORT, ()=>{
 
